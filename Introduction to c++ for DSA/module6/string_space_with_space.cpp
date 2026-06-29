2bcf425fb932c1363b3dead5b7914a98eb0e072a
Anas-Gazi <gazianas2020@gmail.com>
2026-06-29 15:24:25 +0600

    Some New Cpde

---
+++ Introduction to c++ for DSA/module6/string_space_with_space.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a;
+    cin >>  a;
+    string s;
+    // cin>>s;
+    cin.ignore();
+    getline(cin, s);
+
+
+    cout<<a << endl;
+    cout<< s;
+
+       return 0;
+}
